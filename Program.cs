2e5d26517a11c641bba4c791929b66915d2b6aa8
Adryelvitoria791 <Adryelyvitoria791@.gmail.com>
2021-08-29 22:13:03 -0300

    Uma das minhas músicas preferidas, adicionei um novo comando para colocar cores em certas frases

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("E a gente canta");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("E a gente dança");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("E a gente não se cansa");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("De ser criança");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("A gente brinca Na nossa velha infância");
+        }
+    }
+}
